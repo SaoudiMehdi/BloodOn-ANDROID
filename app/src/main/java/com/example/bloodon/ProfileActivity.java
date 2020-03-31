ea50c6bcbbf47cce0bb1a04dc49a3f45362468ad
younessmouad <ymouad96@gmail.com>
2020-03-31 21:48:51 +0100

    Update ProfileActivity.java

--- app/src/main/java/com/example/bloodon/ProfileActivity.java
+++ app/src/main/java/com/example/bloodon/ProfileActivity.java
@@ -1,26 +1,40 @@
-package com.example.bloodon;
+package com.example.mybloodon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.os.Debug;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private Spinner spinnerBlood;
+    private TextView userProfName, userProfCin, userBonusValue;
+    private EditText userProfEmail, userProfAdresse, userProfTelephone;
+    private CircleImageView userProfileImage;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-        spinnerBlood = (Spinner) this.findViewById(R.id.spinnerBlood);
-        ArrayList bloodList = new ArrayList();
-        String[] bloodCategories = {"A-", "A+", "B-", "B+", "AB-", "AB+", "O-", "O+"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, bloodCategories);
-       spinnerBlood.setAdapter(adapter);
-    }
+    private DatabaseReference profileUserRef;
+    private FirebaseAuth mAuth;
+
+    private String currentUserId;
+
+    
 }
+
