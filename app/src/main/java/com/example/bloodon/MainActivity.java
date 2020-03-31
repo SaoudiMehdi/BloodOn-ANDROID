867e2a126ceb73dd26d73f701bf759669eb8e50b
SaoudiMehdi <58574228+SaoudiMehdi@users.noreply.github.com>
2020-03-31 22:49:43 +0100

    Update MainActivity

--- app/src/main/java/com/example/bloodon/MainActivity.java
+++ app/src/main/java/com/example/bloodon/MainActivity.java
@@ -1,165 +1,233 @@
 package com.example.bloodon;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import androidx.annotation.NonNull;
-import com.google.android.material.textfield.TextInputEditText;
 
-import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.os.Build;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
+import androidx.recyclerview.widget.RecyclerView;
+
+
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static com.example.bloodon.R.layout.activity_demandes;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button LoginButton;
-
-    private TextInputEditText UserEmail,UserPassword;
-
-    private Button NeedNewAccountLink;
-
-    private TextView ForgetPasswordLink;
-
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private RecyclerView  postListNav;
+    private Toolbar toolBarNav;
+    private CircleImageView imageProfilNav;
+    private TextView profileUserNameNav;
+    private ImageButton addNewPostNav;
     private FirebaseAuth mAuth;
-    private ProgressDialog loadingBar;
-
-    private static final  String TAG ="LoginActivity";
-
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private DatabaseReference userNav,postsNav,likesNav;
+    String currentUserID;
+    Boolean likeCheckNav = false;
 
 
 
-        Window window = this.getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorBl));
-
-        NeedNewAccountLink= findViewById(R.id.register_account_link);
-        UserEmail = findViewById(R.id.login_email);
-        UserPassword = findViewById(R.id.login_password);
-        LoginButton = findViewById(R.id.login_button);
-        ForgetPasswordLink = findViewById(R.id.forget_password_link);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        loadingBar= new ProgressDialog(this);
+    private void afficherUsersDemandes() {
 
 
+        FirebaseRecyclerOptions<demandes> firebaseRecyclerOptions = new FirebaseRecyclerOptions.Builder<demandes>()
+                .setQuery(postsNav, demandes.class)
+                .build();
 
-        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+        FirebaseRecyclerAdapter<demandes, PostsViewHolder> firebaseRecyclerNav = new FirebaseRecyclerAdapter<demandes, PostsViewHolder>
+                (firebaseRecyclerOptions) {
+            @NonNull
             @Override
-            public void onClick(View v) {
-                SendUserToRegisterActivity();
+            public PostsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
             }
-        });
-        LoginButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View v) {
-                SendUserToMainActivity();
-                AllowingUserToLogin();
-            }
-        });
+            protected void onBindViewHolder(@NonNull PostsViewHolder vH, int position, @NonNull demandes model) {
+                final String initial= getRef(position).getKey();
+                vH.setFullname(model.getFullname());
+                vH.setTime(model.getTime());
+                vH.setDate(model.getDate());
+                vH.setDescription(model.getDescription());
+                vH.setProfileimage(model.getProfileimage());
+                vH.setPostimage(model.getPostimage());
+                vH.setLikeButton(initial);
+                vH.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent clickPostIntent = new Intent(MainActivity.this,clickPostNav.class);
+                        clickPostIntent.putExtra("initial",initial);
+                        startActivity(clickPostIntent);
+                    }
+                });
 
-        ForgetPasswordLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                startActivity(new Intent(MainActivity.this,ResetPasswordActivity.class));
-            }
-        });
+                vH.commentButtonNav.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                        Intent commentsIntent = new Intent(MainActivity.this,commentNav.class);
+                        commentsIntent.putExtra("initial",initial);
+                        startActivity(commentsIntent);
 
-        @Override
-        protected void onStart() {
-            super.onStart();
-            FirebaseUser currentUser= mAuth.getCurrentUser();
-            if(currentUser!=null){
-                SendUserToMainActivity();
-            }
-        }
-
-        private void AllowingUserToLogin() {
-            String email=UserEmail.getText().toString();
-            String password = UserPassword.getText().toString();
-            if(TextUtils.isEmpty(email)){
-                Toast.makeText(this, "Please write your Email...", Toast.LENGTH_SHORT).show();
-            }
-            else  if(TextUtils.isEmpty(password)){
-                Toast.makeText(this, "Please write your password...", Toast.LENGTH_SHORT).show();
-            }
-            else{
-                loadingBar.setTitle("Login ");
-                loadingBar.setMessage("Please wait,While we are allowing you to into your account...");
-                loadingBar.setCanceledOnTouchOutside(true);
-                //   loadingBar.show();
+                    }
+                });
 
 
-                mAuth.signInWithEmailAndPassword(email,password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                vH.likeButtonNav.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        likeCheckNav = true;
+                        likesNav.addValueEventListener(new ValueEventListener() {
                             @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    System.out.println("debug()enter task.issucceeful");
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                            {
+                                if(likeCheckNav.equals(true)){
 
-                                    SendUserToMainActivity();
-                                    System.out.println("debug()main activity called");
-                                    Toast.makeText(MainActivity.this, "you are Logged in successfully", Toast.LENGTH_SHORT).show();
-                                    System.out.println("debug()before dissimissing dialog");
-                                    //                     loadingBar.dismiss();
-                                    System.out.println("debug()after dissimissing dialog");
+                                    if(dataSnapshot.child(initial).hasChild(currentUserID))
+                                    {
+                                        likesNav.child(initial).child(currentUserID).removeValue();
+                                        likeCheckNav = false;
+                                    }
+                                    else
+                                    {
+                                        likesNav.child(initial).child(currentUserID).setValue(true);
+                                        likeCheckNav=false;
+                                    }
+                                }
 
-                                }
-                                else {
-                                    String message= task.getException().getMessage();
-                                    Toast.makeText(MainActivity.this, "Error ocurred"+ message, Toast.LENGTH_SHORT).show();
-                                    loadingBar.dismiss();
-                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
                             }
                         });
+                    }
+                });
+
+            }
+        };
+        postListNav.setAdapter(firebaseRecyclerNav);
+
             }
 
+
+
+    }
+
+
+
+
+
+
+   class PostsViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+        ImageButton likeButtonNav,commentButtonNav;
+        TextView nombreLikesNav;
+        int countLikes;
+        String currentUserId;
+        DatabaseReference LikesNav;
+
+        public PostsViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+            mView = itemView;
+
+            likeButtonNav = mView.findViewById(R.id.likeButton);
+            commentButtonNav = mView.findViewById(R.id.commentButton);
+            nombreLikesNav= mView.findViewById(R.id.numberOfLikes);
+
+            LikesNav = FirebaseDatabase.getInstance().getReference().child("Likes");
+            currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
         }
 
-        private void SendUserToMainActivity() {
-            Intent mainIntent= new Intent(MainActivity.this,MainActivity.class);
-            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(mainIntent);
-            finish();
+        public void setLikeButton(final String initial)
+        {
+            LikesNav.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                {
+                    if(dataSnapshot.child(initial).hasChild(currentUserId)){
+                        countLikes= (int) dataSnapshot.child(initial).getChildrenCount();
+                        likeButtonNav.setImageResource(R.drawable.dislike);
+                        nombreLikesNav.setText(Integer.toString(countLikes)+(" Likes"));
+                    }
+                    else{
+                        countLikes= (int) dataSnapshot.child(initial).getChildrenCount();
+                        likeButtonNav.setImageResource(R.drawable.dislike);
+                        nombreLikesNav.setText(  Integer.toString(countLikes)+(" Likes"));
+
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
         }
 
-        private void SendUserToLoginActivity() {
-            Intent mainIntent= new Intent(MainActivity.this,MainActivity.class);
-            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(mainIntent);
-            finish();
+
+
+
+        public void setFullname(String fullname) {
+            TextView username = mView.findViewById(R.id.userNameNav);
+            username.setText(fullname);
         }
 
+        public void setProfileimage(String profileimage) {
+            CircleImageView image = mView.findViewById(R.id.profileImageNav);
+            Picasso.get().load(profileimage).into(image);
+        }
 
-        private void SendUserToRegisterActivity() {
-            Intent registerIntent = new Intent(MainActivity.this,RegisterActivity.class);
-            startActivity(registerIntent);
+        public void setTime(String time) {
+            TextView PostTime = mView.findViewById(R.id.postTimeNav);
+            PostTime.setText("   " + time);
+        }
+
+        public void setDate(String date) {
+            TextView PostDate = mView.findViewById(R.id.postDateNav);
+            PostDate.setText("   " + date);
+        }
+
+        public void setDescription(String description) {
+            TextView PostDescription = mView.findViewById(R.id.descriptionPostNav);
+            PostDescription.setText(description);
+        }
+
+        public void setPostimage(String postimage) {
+            ImageView PostImage = mView.findViewById(R.id.post_image);
+            Picasso.get().load(postimage).into(PostImage);
         }
     }
+
+
+
+
+
+
+
