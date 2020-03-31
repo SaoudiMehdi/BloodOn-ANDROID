a47f12ccb83888c13c1773876d1f70ace1e2c219
SaoudiMehdi <58574228+SaoudiMehdi@users.noreply.github.com>
2020-03-31 22:50:04 +0100

    CommentNav et clickPostNav (a faire)

---
+++ app/src/main/java/com/example/bloodon/clickPostNav.java
@@ -0,0 +1,32 @@
+package com.example.bloodon;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class clickPostNav extends AppCompatActivity {
+
+
+    
+}
---
+++ app/src/main/java/com/example/bloodon/commentNav.java
@@ -0,0 +1,4 @@
+package com.example.bloodon;
+
+public class commentNav {
+}
